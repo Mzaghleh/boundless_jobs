4879a368effe5f1a6b1aa190708f6b136a7053b2
Mzaghleh <muna_F_89@yahoo.com>
2017-08-27 15:03:38 +0300

    Update jobs_management_spec.rb

--- spec/features/jobs_management_spec.rb
+++ spec/features/jobs_management_spec.rb
@@ -1,7 +1,31 @@
+
 require 'rails_helper'
 
 RSpec.feature 'jobs management', type: :feature do
   scenario 'User creates a new job' do
     skip('add scenario for posting a new job')
+
+    visit "https://boundless-qc.herokuapp.com/jobs"
+    click_button 'New Job'
+    expect(page).to have_content 
+    Fill in
+    Fill in
+    Fill in
+    Fill in
+    expect(page).to have_content 
+
+  scenario 'User Show the newly created job' do
+
+    
+    
+  scenario 'User Edit the newly created job' do
+    
+    
+     
+  scenario 'User Destroy the newly created job' do
+
+
+    
+    
   end
 end
